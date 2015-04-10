733391014f210117db0c74793db36b700db7919b
papplampe <papplampe@github.com>
2015-04-10 10:21:20 +0200

    5: function inlined

--- 5.go
+++ 5.go
@@ -4,29 +4,24 @@ import (
 	"fmt"
 )
 
-func isDivisableByRange(x, s, e uint64) bool {
-	for i := e; i >= s; i-- {
-		if x%i != 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func main() {
 	const (
 		S = 1
 		E = 20
 	)
-	var i, max uint64
+	var i, e, max uint64
 	max = S
 	for i = S + 1; i <= E; i++ {
 		max *= i
 	}
+outer:
 	for i = E; i < max; i++ {
-		if isDivisableByRange(i, S+1, E) {
-			fmt.Println(i)
-			break
+		for e = E; e > S; e-- {
+			if i%e != 0 {
+				continue outer
+			}
 		}
+		fmt.Println(i)
+		break
 	}
 }
